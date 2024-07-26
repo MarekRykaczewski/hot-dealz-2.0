aa4164cf80cd1587453802f9f8ab44ea7576d2cf
Marek <m.rykaczewski99@gmail.com>
2024-07-26 10:27:52 +0200

    fix link (change from relative)

--- components/user-profile-link.tsx
+++ components/user-profile-link.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const UserProfileLink = ({ username }: { username: string }) => {
   return (
-    <Link className="flex items-center gap-2" href={`profile/${username}`}>
+    <Link className="flex items-center gap-2" href={`/profile/${username}`}>
       <div className="bg-gray-300 flex items-center justify-center rounded-full h-10 w-10">
         <span>{username.charAt(0).toUpperCase()}</span>
       </div>
