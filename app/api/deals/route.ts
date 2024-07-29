0bcd3f14b67ad4bd47af4d621bdf45e206149de8
Marek <m.rykaczewski99@gmail.com>
2024-07-29 15:32:46 +0200

    include imageUrls

--- app/api/deals/route.ts
+++ app/api/deals/route.ts
@@ -11,6 +11,7 @@ export async function POST(req: Request) {
       description,
       promoCode,
       price,
+      imageUrls,
       nextBestPrice,
       shippingPrice,
       categoryId,
@@ -27,6 +28,7 @@ export async function POST(req: Request) {
         userId,
         title,
         link,
+        imageUrls,
         description,
         promoCode,
         price,
@@ -43,55 +45,6 @@ export async function POST(req: Request) {
     return NextResponse.json(deal);
   } catch (error) {
     console.log("[DEALS]", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
-
-export async function PATCH(
-  req: Request,
-  { params }: { params: { dealId: string } }
-) {
-  try {
-    const { userId } = auth();
-    const {
-      title,
-      link,
-      description,
-      promoCode,
-      price,
-      imageUrls,
-      nextBestPrice,
-      shippingPrice,
-      categoryId,
-      startDate,
-      endDate,
-    } = await req.json();
-
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    const deal = await db.deal.update({
-      where: { id: params.dealId },
-      data: {
-        userId,
-        title,
-        link,
-        imageUrls,
-        description,
-        promoCode,
-        price,
-        nextBestPrice,
-        shippingPrice,
-        categoryId,
-        startDate,
-        endDate,
-      },
-    });
-
-    return NextResponse.json(deal);
-  } catch (error) {
-    console.log("[UPDATE DEAL]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
