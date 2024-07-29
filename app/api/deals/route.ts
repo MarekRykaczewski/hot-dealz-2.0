edd0a42c9e5c36bca2778d56e5b70afa7cf7974b
Marek <m.rykaczewski99@gmail.com>
2024-07-29 15:31:16 +0200

    restore POST method

--- app/api/deals/route.ts
+++ app/api/deals/route.ts
@@ -2,6 +2,51 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const {
+      title,
+      link,
+      description,
+      promoCode,
+      price,
+      nextBestPrice,
+      shippingPrice,
+      categoryId,
+      startDate,
+      endDate,
+    } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const deal = await db.deal.create({
+      data: {
+        userId,
+        title,
+        link,
+        description,
+        promoCode,
+        price,
+        nextBestPrice,
+        shippingPrice,
+        categoryId,
+        startDate,
+        endDate,
+        score: 0,
+        isPublished: true,
+      },
+    });
+
+    return NextResponse.json(deal);
+  } catch (error) {
+    console.log("[DEALS]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { dealId: string } }
