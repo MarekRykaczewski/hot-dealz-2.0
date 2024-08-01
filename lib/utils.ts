1bd63ecc62e7178db950e691700403c4dfd1f721
Marek <m.rykaczewski99@gmail.com>
2024-08-01 15:31:54 +0200

    remove buildDealsQuery from utils file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,6 @@
-import { Prisma } from "@prisma/client";
 import { type ClassValue, clsx } from "clsx";
 import toast from "react-hot-toast";
 import { twMerge } from "tailwind-merge";
-import { db } from "./db";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -18,65 +16,3 @@ export const handleCopyToClipboard = (text: string) => {
     }
   );
 };
-
-export async function buildDealsQuery({
-  page,
-  pageSize,
-  sortBy,
-  categoryFilter,
-}: {
-  page: number;
-  pageSize: number;
-  sortBy: string;
-  categoryFilter: string;
-}): Promise<Prisma.DealFindManyArgs> {
-  let orderBy: { [key: string]: any } = {};
-
-  if (sortBy === "score") {
-    orderBy = { score: "desc" };
-  } else if (sortBy === "comments") {
-    orderBy = { comments: { _count: "desc" } };
-  }
-
-  const dealsQuery: Prisma.DealFindManyArgs = {
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-    include: {
-      comments: {
-        select: {
-          id: true,
-        },
-      },
-      user: true,
-    },
-    orderBy,
-  };
-
-  if (categoryFilter) {
-    const category = await db.category.findFirst({
-      where: {
-        name: {
-          equals: categoryFilter,
-          mode: "insensitive",
-        },
-      },
-      include: {
-        subcategories: true,
-      },
-    });
-
-    if (category) {
-      const categoryIds = [
-        category.id,
-        ...(category.subcategories.map((sub) => sub.id) || []),
-      ];
-      dealsQuery.where = {
-        categoryId: {
-          in: categoryIds,
-        },
-      };
-    }
-  }
-
-  return dealsQuery;
-}
