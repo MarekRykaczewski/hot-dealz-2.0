5d85b08aecf6674d0342824ac1f63136fecf9582
Marek <m.rykaczewski99@gmail.com>
2024-08-04 09:52:27 +0200

    adjust styles

--- app/(home)/_components/search-item.tsx
+++ app/(home)/_components/search-item.tsx
@@ -10,7 +10,7 @@ const SearchItem = ({ deal }: { deal: Deal }) => {
   return (
     <div className="p-3 w-full hover:bg-slate-200">
       <Link href={`/deals/${deal.id}`} className="flex justify-start">
-        <div className="relative mr-3 w-20 p-1 rounded-lg flex items-center justify-center gap-3">
+        <div className="relative bg-gray-100 border mr-3 w-16 h-16 p-1 rounded-lg flex items-center justify-center gap-3">
           {deal.imageUrls[0] && (
             <Image
               src={deal.imageUrls[0]}
@@ -21,7 +21,7 @@ const SearchItem = ({ deal }: { deal: Deal }) => {
           )}
         </div>
         <div className="flex flex-col w-full gap-2 justify-between">
-          <div className="flex gap-1 font-semibold text-slate-800 text-ellipsis">
+          <div className="flex gap-1 text-sm font-semibold text-slate-800 text-ellipsis">
             <span className="text-center text-orange-500">{deal.score}</span>
             <span>{deal.title}</span>
           </div>
